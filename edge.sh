d10e2472aeda0e6fa77117c079b33b83962e45d4
L3rd <mazzeia@gmail.com>
2014-11-15 11:19:11 +0100

    Update edge.sh

--- edge.sh
+++ edge.sh
@@ -5,7 +5,7 @@ if [ -z "$EDGE" ]; then
   echo "Bleeding edge not requested"
 else
   apt-get install -qy git
-  rm -rf /opt/sickbeard
+  rm -rf /opt/sickbeard-ita
   git clone https://github.com/gborri/Sick-Beard.git /opt/sickbeard-ita
   chown -R nobody:users /opt/sickbeard-ita
 fi
